1ea3b3b5180ca990e06242890d2a4a7e277be092
Abel Chalier <abel@wavy.fr>
2018-08-09 15:57:01 +0200

    lint + factory

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 import {
   NativeModules,
   TextInput,
   findNodeHandle,
-  AppRegistry,
+  AppRegistry
 } from 'react-native';
+import PropTypes from 'prop-types'
 
-const { CustomKeyboard} = NativeModules;
+const { CustomKeyboard } = NativeModules;
 
 const {
-  install, uninstall,
+  install, uninstall, getSelectionRange,
   insertText, backSpace, doDelete,
   moveLeft, moveRight,
   switchSystemKeyboard,
 } = CustomKeyboard;
 
 export {
-  install, uninstall,
+  install, uninstall, getSelectionRange,
   insertText, backSpace, doDelete,
   moveLeft, moveRight,
   switchSystemKeyboard,
 };
 
 const keyboardTypeRegistry = {};
+const defaultKeyboardHeight = 216
 
-export function register(type, factory) {
-  keyboardTypeRegistry[type] = factory;
+export function register(type, keyboardInfo) {
+  keyboardTypeRegistry[type] = keyboardInfo;
+}
+const getKeyboardHeightByType = (type) => {
+  const height = keyboardTypeRegistry[type].height
+  return height || defaultKeyboardHeight
 }
 
 class CustomKeyboardContainer extends Component {
   render() {
-    const {tag, type} = this.props;
-    const factory = keyboardTypeRegistry[type];
+    const { tag, type } = this.props;
+    const factory = keyboardTypeRegistry[type].factory;
+    const inputFilter = keyboardTypeRegistry[type].inputFilter
     if (!factory) {
       console.warn(`Custom keyboard type ${type} not registered.`);
       return null;
     }
     const Comp = factory();
-    return <Comp tag={tag} />;
+    return <Comp tag={tag} inputFilter={inputFilter} />;
   }
 }
 
-AppRegistry.registerComponent("CustomKeyboard", ()=>CustomKeyboardContainer);
+AppRegistry.registerComponent("CustomKeyboard", () => CustomKeyboardContainer);
 
 export class CustomTextInput extends Component {
   static propTypes = {
@@ -51,18 +58,30 @@ export class CustomTextInput extends Component {
     customKeyboardType: PropTypes.string,
   };
   componentDidMount() {
-    install(findNodeHandle(this.input), this.props.customKeyboardType);
+    install(
+      findNodeHandle(this.input),
+      this.props.customKeyboardType,
+      this.props.maxLength === undefined ? 1024 : this.props.maxLength,
+      getKeyboardHeightByType(this.props.customKeyboardType)
+    );
   }
   componentWillReceiveProps(newProps) {
-    if (newProps.customKeyboardType !== this.props.customKeyboardType) {
-      install(findNodeHandle(this.input), newProps.customKeyboardType);
+    if (this.props.customKeyboardType && newProps.customKeyboardType && newProps.customKeyboardType !== this.props.customKeyboardType) {
+      install(
+        findNodeHandle(this.input),
+        newProps.customKeyboardType,
+        newProps.maxLength === undefined ? 1024 : this.props.maxLength,
+        getKeyboardHeightByType(newProps.customKeyboardType)
+      );
     }
   }
   onRef = ref => {
-    this.input = ref;
+    if (ref) {
+      this.input = ref;
+    }
   };
   render() {
     const { customKeyboardType, ...others } = this.props;
-    return <TextInput {...others} ref={this.onRef}/>;
+    return <TextInput {...others} keyboardType={'numeric'} ref={this.onRef} />;
   }
 }
